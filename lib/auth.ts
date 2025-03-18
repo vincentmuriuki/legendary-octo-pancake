2a7b9f026e14a1d1a1c5b102046e3709b0912c44
Vincent Muriuki <vincent@smartmile.eu>
2025-03-18 12:11:27 +0300

    implement the login functionality
    - check whether credentials have been passed and respond appropriately
    - Check whether user exists
    - Finally check whether password matches

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import prisma from './prisma'
 import CredentialsProvider from 'next-auth/providers/credentials'
+import bcrypt from 'bcryptjs'
 
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
@@ -21,7 +22,23 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
                 }
             },
             async authorize(credentials) {
-                
+                if (!credentials?.email || !credentials.password) return null
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if (!user) return null;
+
+                const validPwd = await bcrypt.compare(credentials.password, user.password)
+                if (!validPwd) return null;
+
+                return {
+                    id: user.id,
+                    email: user.email
+                }
             }
         })
     ]
